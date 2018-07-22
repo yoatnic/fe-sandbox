1fbbb8aace8f28a66a366f68b7b1b0a35c20a516
yoat <yoatnic@me.com>
2018-07-22 21:01:00 +0900

    add sw

--- src/index.js
+++ src/index.js
@@ -6,6 +6,22 @@ import { store } from "./Store";
 import RootContainer from "./components/containers/RootContainer";
 import "./styles/index.css";
 
+if ("serviceWorker" in navigator) {
+  window.navigator.serviceWorker
+    .register("/sw.js")
+    .then(function(registration) {
+      // 登録成功
+      console.log(
+        "ServiceWorker registration successful with scope: ",
+        registration.scope
+      );
+    })
+    .catch(function(err) {
+      // 登録失敗 :(
+      console.log("ServiceWorker registration failed: ", err);
+    });
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <RootContainer />
