8c41288c3cb3f213b109ee62f36e2a60f0a4ae7d
yoat <yoatnic@me.com>
2018-08-03 21:37:44 +0900

    add table (no styling)

--- src/components/pages/Blueprint.js
+++ src/components/pages/Blueprint.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Button, Intent, Spinner } from "@blueprintjs/core";
+import { Table, Column, Cell } from "@blueprintjs/table";
 import "normalize.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 
 const Blueprint = () => {
+  const cellRenderer = (rowIndex: number) => {
+    return <Cell>{`$${(rowIndex * 10).toFixed(2)}`}</Cell>;
+  };
   return (
     <div>
       <Spinner intent={Intent.PRIMARY} />
       <Button intent={Intent.SUCCESS}>button</Button>
+      <Table numRows={10}>
+        <Column name="Dollars" cellRenderer={cellRenderer} />
+      </Table>
     </div>
   );
 };
